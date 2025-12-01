99ffd7ca044f9d335cedfcef0674ec40ce739875
Subilan <suyuanliang1995@gmail.com>
2025-12-01 21:12:48 +0800

    using camel case

--- helpers/resp.go
+++ helpers/resp.go
@@ -3,13 +3,13 @@ package helpers
 import "github.com/gin-gonic/gin"
 
 func Details(str string) gin.H {
-	return gin.H{"Details": str}
+	return gin.H{"details": str}
 }
 
 func Data(d any) gin.H {
-	return gin.H{"Data": d}
+	return gin.H{"data": d}
 }
 
 func PaginData(d any, cnt int, nextToken string) gin.H {
-	return gin.H{"Data": d, "cnt": cnt, "nextToken": nextToken}
+	return gin.H{"data": d, "cnt": cnt, "nextToken": nextToken}
 }
