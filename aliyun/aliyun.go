d4b8653cced3d2223854271e5d9eefc18c70d818
Subilan <1843917482@qq.com>
2026-04-01 17:13:41 +0800

    chore

--- aliyun/aliyun.go
+++ aliyun/aliyun.go
@@ -1,5 +1,11 @@
 package aliyun
 
+import (
+	"go-aliyunmc/logs"
+
+	"github.com/aliyun/credentials-go/credentials"
+)
+
 // MustInitialize 初始化各个阿里云client
 func MustInitialize() {
 	var err error
@@ -17,4 +23,20 @@ func MustInitialize() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+
+// MustGetAKCredential 返回从配置中提取的 AK 凭证，如果无法提取则 panic
+func MustGetAKCredential() credentials.Credential {
+	crConfig := new(credentials.Config).
+		SetType("access_key").
+		SetAccessKeyId(C.AccessKeyId).
+		SetAccessKeySecret(C.AccessKeySecret)
+	cr, err := credentials.NewCredential(crConfig)
+
+	if err != nil {
+		logs.Fatal("无法创建凭证数据")
+	}
+
+	return cr
 }
