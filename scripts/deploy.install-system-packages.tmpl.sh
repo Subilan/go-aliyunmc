5f8437c76d962b5c97e2a1a204cc3f59fd53d440
Subilan <1843917482@qq.com>
2026-04-07 13:20:46 +0800

    changing from zulu to adoptium

--- scripts/deploy.install-system-packages.tmpl.sh
+++ scripts/deploy.install-system-packages.tmpl.sh
@@ -4,4 +4,4 @@ export MAN_DISABLE_CACHE=yes
 export DEBIAN_FRONTEND=noninteractive
 
 echo "安装系统软件"
-apt install -y zulu{{ .JavaVersion }}-jre-headless {{ range .Packages }}{{ . }} {{ end }}
+apt install -y temurin-{{ .JavaVersion }}-jre {{ range .Packages }}{{ . }} {{ end }}
