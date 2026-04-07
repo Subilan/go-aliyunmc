f84e687717afe68da3c3b8df88fbfdcbf30e8f69
Subilan <1843917482@qq.com>
2026-04-07 15:18:02 +0800

    update cp usage

--- scripts/deploy.restore-archive-data.tmpl.sh
+++ scripts/deploy.restore-archive-data.tmpl.sh
@@ -6,10 +6,8 @@ USER_HOME="/home/${USERNAME}"
 
 echo "复制归档数据"
 
-ossutil cp -r "{{ .ArchiveOSSPath }}" "${USER_HOME}/server"
+ossutil cp --parallel=5 --jobs=20 -r "{{ .ArchiveOSSPath }}/" "${USER_HOME}/server/archive"
 chmod +x "${USER_HOME}/server/archive/boot.sh"
 chmod +x "${USER_HOME}/server/archive/start.sh"
 
 chown -R "${USERNAME}:${USERNAME}" "${USER_HOME}"
-
-echo "===== 部署成功完成 ====="
