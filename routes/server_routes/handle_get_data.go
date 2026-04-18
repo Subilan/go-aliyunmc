7662c09f47775fb54b38e348a46277d90444ab0d
Subilan <1843917482@qq.com>
2026-04-18 21:55:32 +0800

    get server data routes

---
+++ routes/server_routes/handle_get_data.go
@@ -0,0 +1,77 @@
+package server_routes
+
+import (
+	"encoding/json"
+	"go-aliyunmc/h"
+	"go-aliyunmc/monitors"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/goccy/go-yaml"
+)
+
+var supportedFileSuffix = [...]string{"json", "yml", "yaml"}
+var supportedFileHandlers map[string]func([]byte) (map[string]any, error)
+
+func init() {
+	jsonHandler := func(data []byte) (map[string]any, error) {
+		var result map[string]any
+		err := json.Unmarshal(data, &result)
+		return result, err
+	}
+
+	yamlHandler := func(data []byte) (map[string]any, error) {
+		var result map[string]any
+		err := yaml.Unmarshal(data, &result)
+		return result, err
+	}
+
+	supportedFileHandlers = map[string]func([]byte) (map[string]any, error){
+		"json": jsonHandler,
+		"yaml": yamlHandler,
+		"yml":  yamlHandler,
+	}
+}
+
+func HandleGetData(c *gin.Context) (any, error) {
+	filename := c.Query("filename")
+
+	if filename == "" {
+		return nil, h.HttpError(http.StatusBadRequest, "请提供文件名")
+	}
+
+	for _, file := range monitors.FileSyncC.Files {
+		fileSuffix := ""
+		for _, suffix := range supportedFileSuffix {
+			if strings.HasSuffix(file.LocalPath, "."+suffix) {
+				fileSuffix = suffix
+				break
+			}
+		}
+
+		if fileSuffix == "" {
+			continue
+		}
+
+		if file.Id != "" && file.Id == filename || file.LocalPath == filename {
+			filePath := monitors.FileSyncC.LocalCacheRoot + file.LocalPath
+			content, err := os.ReadFile(filePath)
+
+			if err != nil {
+				return nil, err
+			}
+
+			data, err := supportedFileHandlers[fileSuffix](content)
+
+			if err != nil {
+				return nil, err
+			}
+
+			return data, nil
+		}
+	}
+
+	return nil, h.HttpError(http.StatusNotFound, "未找到指定文件")
+}
