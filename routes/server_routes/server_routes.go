67c6c6d64a643ac41b7d996b2d888b4644fd16a7
Subilan <suyuanliang1995@gmail.com>
2026-04-18 21:55:32 +0800

    get server data routes

--- routes/server_routes/server_routes.go
+++ routes/server_routes/server_routes.go
@@ -2,13 +2,17 @@ package server_routes
 
 import (
 	"go-aliyunmc/h"
+	"go-aliyunmc/mid"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Bind(engine *gin.Engine) {
 	serverGroup := engine.Group("/server")
+	serverGroup.Use(mid.Auth())
+	serverGroup.Use(mid.Enforce())
 	{
 		serverGroup.GET("/stop", h.G(HandleStopServer))
+		serverGroup.GET("/data", h.G(HandleGetData))
 	}
 }
