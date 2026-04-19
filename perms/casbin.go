539d922bb2e248720baabd16f542a6b86a22c2bd
Subilan <suyuanliang1995@gmail.com>
2026-04-19 15:10:01 +0800

    normal role

--- perms/casbin.go
+++ perms/casbin.go
@@ -2,16 +2,21 @@ package perms
 
 import (
 	"go-aliyunmc/log_util"
+
 	"github.com/casbin/casbin/v2"
 )
 
-var En *casbin.Enforcer
+var enforcer *casbin.Enforcer
 
 // MustInitialize 初始化 Casbin 权限管理器
 func MustInitialize() {
-	enforcer, err := casbin.NewEnforcer(C.ModelPath, C.PolicyPath)
+	var err error
+	enforcer, err = casbin.NewEnforcer(C.ModelPath, C.PolicyPath)
 	if err != nil {
 		log_util.Fatal("无法初始化 Casbin 权限管理器: %v", err)
 	}
-	En = enforcer
+}
+
+func Enforce(role Role, obj, act string) (bool, error) {
+	return enforcer.Enforce(role.Normal(), obj, act)
 }
