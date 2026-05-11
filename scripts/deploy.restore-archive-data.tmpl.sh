59e716d090400bacc9ee45d07faeb4dc4b018ae7
Subilan <1843917482@qq.com>
2026-05-11 11:27:16 +0800

    fix: improve reliability

--- scripts/deploy.restore-archive-data.tmpl.sh
+++ scripts/deploy.restore-archive-data.tmpl.sh
@@ -6,7 +6,7 @@ USER_HOME="/home/${USERNAME}"
 
 echo "复制归档数据"
 
-ossutil cp --parallel=5 --jobs=20 -r "{{ .ArchiveOSSPath }}/" "${USER_HOME}/server/archive"
+ossutil cp --parallel=7 --jobs=25 -r -f "{{ .ArchiveOSSPath }}/" "${USER_HOME}/server/archive" > "${USER_HOME}/restore_archive_data.log" 2>&1
 chmod +x "${USER_HOME}/server/archive/boot.sh"
 chmod +x "${USER_HOME}/server/archive/start.sh"
 
